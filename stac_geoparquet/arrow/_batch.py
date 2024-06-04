cf0699b1b3e24b381a2a617a2ccd949a4863f856
Kyle Barron <kyle@developmentseed.org>
2024-06-04 12:46:45 -0400

    Refactor interally to `RawBatch` and `CleanBatch` wrapper types (#57)
    * Move json equality logic outside of test_arrow
    
    * Refactor to RawBatch and CleanBatch wrapper types
    
    * Move _from_arrow functions to _api
    
    * Update imports
    
    * fix circular import
    
    * keep deprecated api
    
    * Add write-read test and fix typing
    
    * add parquet tests
    
    * fix ci
    
    * Rename wrapper types

---
+++ stac_geoparquet/arrow/_batch.py
@@ -0,0 +1,190 @@
+from __future__ import annotations
+
+import os
+from copy import deepcopy
+from pathlib import Path
+from typing import Any, Iterable
+
+import numpy as np
+import orjson
+import pyarrow as pa
+import pyarrow.compute as pc
+import shapely
+import shapely.geometry
+from numpy.typing import NDArray
+from typing_extensions import Self
+
+from stac_geoparquet.arrow._from_arrow import (
+    convert_bbox_to_array,
+    convert_timestamp_columns_to_string,
+    lower_properties_from_top_level,
+)
+from stac_geoparquet.arrow._to_arrow import (
+    assign_geoarrow_metadata,
+    bring_properties_to_top_level,
+    convert_bbox_to_struct,
+    convert_timestamp_columns,
+)
+from stac_geoparquet.arrow._util import convert_tuples_to_lists, set_by_path
+
+
+class StacJsonBatch:
+    """
+    An Arrow RecordBatch of STAC Items that has been **minimally converted** to Arrow.
+    That is, it aligns as much as possible to the raw STAC JSON representation.
+
+    The **only** transformations that have already been applied here are those that are
+    necessary to represent the core STAC items in Arrow.
+
+    - `geometry` has been converted to WKB binary
+    - `properties.proj:geometry`, if it exists, has been converted to WKB binary
+      ISO encoding
+    - The `proj:geometry` in any asset properties, if it exists, has been converted to
+      WKB binary.
+
+    No other transformations have yet been applied. I.e. all properties are still in a
+    top-level `properties` struct column.
+    """
+
+    inner: pa.RecordBatch
+    """The underlying pyarrow RecordBatch"""
+
+    def __init__(self, batch: pa.RecordBatch) -> None:
+        self.inner = batch
+
+    @classmethod
+    def from_dicts(
+        cls, items: Iterable[dict[str, Any]], *, schema: pa.Schema | None = None
+    ) -> Self:
+        """Construct a StacJsonBatch from an iterable of dicts representing STAC items.
+
+        All items will be parsed into a single RecordBatch, meaning that each internal
+        array is fully contiguous in memory for the length of `items`.
+
+        Args:
+            items: STAC Items to convert to Arrow
+
+        Kwargs:
+            schema: An optional schema that describes the format of the data. Note that
+                this must represent the geometry column and any `proj:geometry` columns
+                as binary type.
+
+        Returns:
+            a new StacJsonBatch of data.
+        """
+        # Preprocess GeoJSON to WKB in each STAC item
+        # Otherwise, pyarrow will try to parse coordinates into a native geometry type
+        # and if you have multiple geometry types pyarrow will error with
+        # `ArrowInvalid: cannot mix list and non-list, non-null values`
+        wkb_items = []
+        for item in items:
+            wkb_item = deepcopy(item)
+            wkb_item["geometry"] = shapely.to_wkb(
+                shapely.geometry.shape(wkb_item["geometry"]), flavor="iso"
+            )
+
+            # If a proj:geometry key exists in top-level properties, convert that to WKB
+            if "proj:geometry" in wkb_item["properties"]:
+                wkb_item["properties"]["proj:geometry"] = shapely.to_wkb(
+                    shapely.geometry.shape(wkb_item["properties"]["proj:geometry"]),
+                    flavor="iso",
+                )
+
+            # If a proj:geometry key exists in any asset properties, convert that to WKB
+            for asset_value in wkb_item["assets"].values():
+                if "proj:geometry" in asset_value:
+                    asset_value["proj:geometry"] = shapely.to_wkb(
+                        shapely.geometry.shape(asset_value["proj:geometry"]),
+                        flavor="iso",
+                    )
+
+            wkb_items.append(wkb_item)
+
+        if schema is not None:
+            array = pa.array(wkb_items, type=pa.struct(schema))
+        else:
+            array = pa.array(wkb_items)
+
+        return cls(pa.RecordBatch.from_struct_array(array))
+
+    def iter_dicts(self) -> Iterable[dict]:
+        batch = self.inner
+
+        # Find all paths in the schema that have a WKB geometry
+        geometry_paths = [["geometry"]]
+        try:
+            batch.schema.field("properties").type.field("proj:geometry")
+            geometry_paths.append(["properties", "proj:geometry"])
+        except KeyError:
+            pass
+
+        assets_struct = batch.schema.field("assets").type
+        for asset_idx in range(assets_struct.num_fields):
+            asset_field = assets_struct.field(asset_idx)
+            if "proj:geometry" in pa.schema(asset_field).names:
+                geometry_paths.append(["assets", asset_field.name, "proj:geometry"])
+
+        # Convert each geometry column to a Shapely geometry, and then assign the
+        # geojson geometry when converting each row to a dictionary.
+        geometries: list[NDArray[np.object_]] = []
+        for geometry_path in geometry_paths:
+            col = batch
+            for path_segment in geometry_path:
+                if isinstance(col, pa.RecordBatch):
+                    col = col[path_segment]
+                elif pa.types.is_struct(col.type):
+                    col = pc.struct_field(col, path_segment)  # type: ignore
+                else:
+                    raise AssertionError(f"unexpected type {type(col)}")
+
+            geometries.append(shapely.from_wkb(col))
+
+        struct_batch = batch.to_struct_array()
+        for row_idx in range(len(struct_batch)):
+            row_dict = struct_batch[row_idx].as_py()
+            for geometry_path, geometry_column in zip(geometry_paths, geometries):
+                geojson_g = geometry_column[row_idx].__geo_interface__
+                geojson_g["coordinates"] = convert_tuples_to_lists(
+                    geojson_g["coordinates"]
+                )
+                set_by_path(row_dict, geometry_path, geojson_g)
+
+            yield row_dict
+
+    def to_clean_batch(self) -> StacArrowBatch:
+        batch = self.inner
+
+        batch = bring_properties_to_top_level(batch)
+        batch = convert_timestamp_columns(batch)
+        batch = convert_bbox_to_struct(batch)
+        batch = assign_geoarrow_metadata(batch)
+
+        return StacArrowBatch(batch)
+
+    def to_ndjson(self, dest: str | Path | os.PathLike[bytes]) -> None:
+        with open(dest, "ab") as f:
+            for item_dict in self.iter_dicts():
+                f.write(orjson.dumps(item_dict))
+                f.write(b"\n")
+
+
+class StacArrowBatch:
+    """
+    An Arrow RecordBatch of STAC Items that has been processed to match the
+    STAC-GeoParquet specification.
+    """
+
+    inner: pa.RecordBatch
+    """The underlying pyarrow RecordBatch"""
+
+    def __init__(self, batch: pa.RecordBatch) -> None:
+        self.inner = batch
+
+    def to_raw_batch(self) -> StacJsonBatch:
+        batch = self.inner
+
+        batch = convert_timestamp_columns_to_string(batch)
+        batch = lower_properties_from_top_level(batch)
+        batch = convert_bbox_to_array(batch)
+
+        return StacJsonBatch(batch)
--- stac_geoparquet/arrow/_to_arrow.py
+++ stac_geoparquet/arrow/_to_arrow.py
@@ -1,102 +1,197 @@
 """Convert STAC data into Arrow tables"""
 
-from pathlib import Path
-from typing import (
-    Any,
-    Dict,
-    Iterable,
-    Iterator,
-    Optional,
-    Union,
-)
-
+import ciso8601
+import numpy as np
+import orjson
 import pyarrow as pa
+import pyarrow.compute as pc
 
-from stac_geoparquet.arrow._schema.models import InferredSchema
-from stac_geoparquet.json_reader import read_json_chunked
-from stac_geoparquet.arrow._util import stac_items_to_arrow, batched_iter
+from stac_geoparquet.arrow._crs import WGS84_CRS_JSON
 
 
-def parse_stac_items_to_arrow(
-    items: Iterable[Dict[str, Any]],
-    *,
-    chunk_size: int = 8192,
-    schema: Optional[Union[pa.Schema, InferredSchema]] = None,
-) -> Iterable[pa.RecordBatch]:
-    """Parse a collection of STAC Items to an iterable of :class:`pyarrow.RecordBatch`.
+def bring_properties_to_top_level(
+    batch: pa.RecordBatch,
+) -> pa.RecordBatch:
+    """Bring all the fields inside of the nested "properties" struct to the top level"""
+    properties_field = batch.schema.field("properties")
+    properties_column = batch["properties"]
 
-    The objects under `properties` are moved up to the top-level of the
-    Table, similar to :meth:`geopandas.GeoDataFrame.from_features`.
+    for field_idx in range(properties_field.type.num_fields):
+        inner_prop_field = properties_field.type.field(field_idx)
+        batch = batch.append_column(
+            inner_prop_field,
+            pc.struct_field(properties_column, field_idx),  # type: ignore
+        )
+
+    batch = batch.drop_columns(
+        [
+            "properties",
+        ]
+    )
+    return batch
+
+
+def convert_timestamp_columns(
+    batch: pa.RecordBatch,
+) -> pa.RecordBatch:
+    """Convert all timestamp columns from a string to an Arrow Timestamp data type"""
+    allowed_column_names = {
+        "datetime",  # common metadata
+        "start_datetime",
+        "end_datetime",
+        "created",
+        "updated",
+        "expires",  # timestamps extension
+        "published",
+        "unpublished",
+    }
+    for column_name in allowed_column_names:
+        try:
+            column = batch[column_name]
+        except KeyError:
+            continue
+
+        field_index = batch.schema.get_field_index(column_name)
+
+        if pa.types.is_timestamp(column.type):
+            continue
+
+        # STAC allows datetimes to be null. If all rows are null, the column type may be
+        # inferred as null. We cast this to a timestamp column.
+        elif pa.types.is_null(column.type):
+            batch = batch.set_column(
+                field_index, column_name, column.cast(pa.timestamp("us"))
+            )
+
+        elif pa.types.is_string(column.type):
+            batch = batch.set_column(
+                field_index, column_name, _convert_single_timestamp_column(column)
+            )
+        else:
+            raise ValueError(
+                f"Inferred time column '{column_name}' was expected to be a string or"
+                f" timestamp data type but got {column.type}"
+            )
+
+    return batch
+
+
+def _convert_single_timestamp_column(column: pa.Array) -> pa.TimestampArray:
+    """Convert an individual timestamp column from string to a Timestamp type"""
+    return pa.array(
+        [ciso8601.parse_rfc3339(str(t)) for t in column], pa.timestamp("us", tz="UTC")
+    )
+
+
+def _is_bbox_3d(bbox_col: pa.Array) -> bool:
+    """Infer whether the bounding box column represents 2d or 3d bounding boxes."""
+    offsets_set = set()
+    offsets = bbox_col.offsets.to_numpy()
+    offsets_set.update(np.unique(offsets[1:] - offsets[:-1]))
+
+    if len(offsets_set) > 1:
+        raise ValueError("Mixed 2d-3d bounding boxes not yet supported")
+
+    offset = list(offsets_set)[0]
+    if offset == 6:
+        return True
+    elif offset == 4:
+        return False
+    else:
+        raise ValueError(f"Unexpected bbox offset: {offset=}")
+
+
+def convert_bbox_to_struct(batch: pa.RecordBatch) -> pa.RecordBatch:
+    """Convert bbox column to a struct representation
+
+    Since the bbox in JSON is stored as an array, pyarrow automatically converts the
+    bbox column to a ListArray. But according to GeoParquet 1.1, we should save the bbox
+    column as a StructArray, which allows for Parquet statistics to infer any spatial
+    partitioning in the dataset.
 
     Args:
-        items: the STAC Items to convert
-        chunk_size: The chunk size to use for Arrow record batches. This only takes
-            effect if `schema` is not None. When `schema` is None, the input will be
-            parsed into a single contiguous record batch. Defaults to 8192.
-        schema: The schema of the input data. If provided, can improve memory use;
-            otherwise all items need to be parsed into a single array for schema
-            inference. Defaults to None.
+        batch: _description_
 
     Returns:
-        an iterable of pyarrow RecordBatches with the STAC-GeoParquet representation of items.
+        New record batch
     """
-    if schema is not None:
-        if isinstance(schema, InferredSchema):
-            schema = schema.inner
+    bbox_col_idx = batch.schema.get_field_index("bbox")
+    bbox_col = batch.column(bbox_col_idx)
+    bbox_3d = _is_bbox_3d(bbox_col)
 
-        # If schema is provided, then for better memory usage we parse input STAC items
-        # to Arrow batches in chunks.
-        for chunk in batched_iter(items, chunk_size):
-            yield stac_items_to_arrow(chunk, schema=schema)
+    assert (
+        pa.types.is_list(bbox_col.type)
+        or pa.types.is_large_list(bbox_col.type)
+        or pa.types.is_fixed_size_list(bbox_col.type)
+    )
+    if bbox_3d:
+        coords = bbox_col.flatten().to_numpy().reshape(-1, 6)
+    else:
+        coords = bbox_col.flatten().to_numpy().reshape(-1, 4)
+
+    if bbox_3d:
+        xmin = coords[:, 0]
+        ymin = coords[:, 1]
+        zmin = coords[:, 2]
+        xmax = coords[:, 3]
+        ymax = coords[:, 4]
+        zmax = coords[:, 5]
+
+        struct_arr = pa.StructArray.from_arrays(
+            [
+                xmin,
+                ymin,
+                zmin,
+                xmax,
+                ymax,
+                zmax,
+            ],
+            names=[
+                "xmin",
+                "ymin",
+                "zmin",
+                "xmax",
+                "ymax",
+                "zmax",
+            ],
+        )
 
     else:
-        # If schema is _not_ provided, then we must convert to Arrow all at once, or
-        # else it would be possible for a STAC item late in the collection (after the
-        # first chunk) to have a different schema and not match the schema inferred for
-        # the first chunk.
-        yield stac_items_to_arrow(items)
+        xmin = coords[:, 0]
+        ymin = coords[:, 1]
+        xmax = coords[:, 2]
+        ymax = coords[:, 3]
 
-
-def parse_stac_ndjson_to_arrow(
-    path: Union[str, Path, Iterable[Union[str, Path]]],
-    *,
-    chunk_size: int = 65536,
-    schema: Optional[pa.Schema] = None,
-    limit: Optional[int] = None,
-) -> Iterator[pa.RecordBatch]:
-    """
-    Convert one or more newline-delimited JSON STAC files to a generator of Arrow
-    RecordBatches.
-
-    Each RecordBatch in the returned iterator is guaranteed to have an identical schema,
-    and can be used to write to one or more Parquet files.
-
-    Args:
-        path: One or more paths to files with STAC items.
-        chunk_size: The chunk size. Defaults to 65536.
-        schema: The schema to represent the input STAC data. Defaults to None, in which
-            case the schema will first be inferred via a full pass over the input data.
-            In this case, there will be two full passes over the input data: one to
-            infer a common schema across all data and another to read the data.
-
-    Other args:
-        limit: The maximum number of JSON Items to use for schema inference
-
-    Yields:
-        Arrow RecordBatch with a single chunk of Item data.
-    """
-    # If the schema was not provided, then we need to load all data into memory at once
-    # to perform schema resolution.
-    if schema is None:
-        inferred_schema = InferredSchema()
-        inferred_schema.update_from_json(path, chunk_size=chunk_size, limit=limit)
-        yield from parse_stac_ndjson_to_arrow(
-            path, chunk_size=chunk_size, schema=inferred_schema
+        struct_arr = pa.StructArray.from_arrays(
+            [
+                xmin,
+                ymin,
+                xmax,
+                ymax,
+            ],
+            names=[
+                "xmin",
+                "ymin",
+                "xmax",
+                "ymax",
+            ],
         )
-        return
 
-    if isinstance(schema, InferredSchema):
-        schema = schema.inner
+    return batch.set_column(bbox_col_idx, "bbox", struct_arr)
 
-    for batch in read_json_chunked(path, chunk_size=chunk_size):
-        yield stac_items_to_arrow(batch, schema=schema)
+
+def assign_geoarrow_metadata(
+    batch: pa.RecordBatch,
+) -> pa.RecordBatch:
+    """Tag the primary geometry column with `geoarrow.wkb` on the field metadata."""
+    existing_field_idx = batch.schema.get_field_index("geometry")
+    existing_field = batch.schema.field(existing_field_idx)
+    ext_metadata = {"crs": WGS84_CRS_JSON}
+    field_metadata = {
+        b"ARROW:extension:name": b"geoarrow.wkb",
+        b"ARROW:extension:metadata": orjson.dumps(ext_metadata),
+    }
+    new_field = existing_field.with_metadata(field_metadata)
+    return batch.set_column(
+        existing_field_idx, new_field, batch.column(existing_field_idx)
+    )
