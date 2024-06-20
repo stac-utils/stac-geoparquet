11451636cd1798dbb548118d900be883f09f2762
Kyle Barron <kyle@developmentseed.org>
2024-06-20 16:39:15 -0400

    fix typing

--- stac_geoparquet/arrow/_constants.py
+++ stac_geoparquet/arrow/_constants.py
@@ -6,5 +6,5 @@ DEFAULT_JSON_CHUNK_SIZE = 65536
 SUPPORTED_PARQUET_SCHEMA_VERSIONS = Literal["1.0.0", "1.1.0"]
 """A Literal type with the supported GeoParquet schema versions."""
 
-DEFAULT_PARQUET_SCHEMA_VERSION = "1.1.0"
+DEFAULT_PARQUET_SCHEMA_VERSION: Literal["1.0.0", "1.1.0"] = "1.1.0"
 """The default GeoParquet schema version written to file."""
