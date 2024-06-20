ff065d2b344b78c1ef572d2d55ec47a8f1d3bbdd
Kyle Barron <kyle@developmentseed.org>
2024-06-20 16:14:37 -0400

    export constants in public api

--- stac_geoparquet/arrow/__init__.py
+++ stac_geoparquet/arrow/__init__.py
@@ -4,4 +4,9 @@ from ._api import (
     stac_table_to_items,
     stac_table_to_ndjson,
 )
+from ._constants import (
+    DEFAULT_JSON_CHUNK_SIZE,
+    DEFAULT_PARQUET_SCHEMA_VERSION,
+    SUPPORTED_PARQUET_SCHEMA_VERSIONS,
+)
 from ._to_parquet import parse_stac_ndjson_to_parquet, to_parquet
