5ee47461b3d561d7ba359f25ce4bae0e658b6ad8
Kyle Barron <kyle@developmentseed.org>
2024-06-21 14:36:49 -0400

    Add arrow-based API docs

--- stac_geoparquet/arrow/__init__.py
+++ stac_geoparquet/arrow/__init__.py
@@ -11,3 +11,16 @@ from ._constants import (
 )
 from ._delta_lake import parse_stac_ndjson_to_delta_lake
 from ._to_parquet import parse_stac_ndjson_to_parquet, to_parquet
+
+__all__ = (
+    "DEFAULT_JSON_CHUNK_SIZE",
+    "DEFAULT_PARQUET_SCHEMA_VERSION",
+    "parse_stac_items_to_arrow",
+    "parse_stac_ndjson_to_arrow",
+    "parse_stac_ndjson_to_delta_lake",
+    "parse_stac_ndjson_to_parquet",
+    "stac_table_to_items",
+    "stac_table_to_ndjson",
+    "SUPPORTED_PARQUET_SCHEMA_VERSIONS",
+    "to_parquet",
+)
