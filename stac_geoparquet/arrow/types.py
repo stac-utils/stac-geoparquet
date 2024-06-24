93e187b0b81c57623a8b73374b539afe4bf93ebc
Kyle Barron <kyle@developmentseed.org>
2024-06-24 14:21:49 -0400

    flake8 lint

--- stac_geoparquet/arrow/types.py
+++ stac_geoparquet/arrow/types.py
@@ -2,4 +2,4 @@ from typing import Protocol
 
 
 class ArrowStreamExportable(Protocol):
-    def __arrow_c_stream__(self, requested_schema: object | None = None) -> object: ...
+    def __arrow_c_stream__(self, requested_schema: object | None = None) -> object: ...  # noqa
